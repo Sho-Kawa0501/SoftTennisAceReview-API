76c98b2623977987836044d07313ca648daa79b8
Sho-Kawa0501 <akirasho0501@gmail.com>
2024-01-27 23:27:11 +0900

    [update] settings.pyのトークン期限を適切な値に変更

--- reviewsite/settings.py
+++ reviewsite/settings.py
@@ -169,7 +169,7 @@ JWT_AUTH = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
